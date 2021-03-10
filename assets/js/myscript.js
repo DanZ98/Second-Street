2b0689f9095bce03ae7375baaf629b3369a7c0e7
Dan Zins <danzins15@gmail.com>
2021-03-10 17:29:03 -0600

    Completed header nav section

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,13 +1,22 @@
-// retrieving hamgburger menu
+// retrieving elements/classes
 const hamRotate = document.querySelector('#ham-rotate');
 const hamDisplay = document.querySelector('#ham-display');
+const arrow = document.querySelector('#arrow');
+const subUnorderedList = document.querySelector('.subunordered--display');
 
-// execute ham rotate/display click function and toggle class
+
+// executing ham rotate/display click function and toggle class
 hamRotate.addEventListener('click', function(){
     hamRotate.classList.toggle('rotated-ham');
     hamDisplay.classList.toggle('nav-heading__mobile-navlist--displaynone');
-    
 });
+
+// executing arrow display sub list and toggle arrow rotate class
+arrow.addEventListener('click', function(){
+    subUnorderedList.classList.toggle('subunordered--display');
+    arrow.classList.toggle('arrow-rotate');
+});
+
 
 
 
