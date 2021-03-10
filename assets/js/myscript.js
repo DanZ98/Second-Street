bce10d94b9280ba665581bd8010e5ca6d2ca43c6
Dan Zins <danzins15@gmail.com>
2021-03-09 21:36:29 -0600

    Completed nav heading section, toggle hamburger menu in vanilla JavaScript

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,5 +1,13 @@
+// retrieving hamgburger menu
+const hamRotate = document.querySelector('#ham-rotate');
+const hamDisplay = document.querySelector('#ham-display');
 
-
+// execute ham rotate/display click function and toggle class
+hamRotate.addEventListener('click', function(){
+    hamRotate.classList.toggle('rotated-ham');
+    hamDisplay.classList.toggle('nav-heading__mobile-navlist--displaynone');
+    
+});
 
 
 
