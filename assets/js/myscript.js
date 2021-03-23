78c8df31ec64cc3d0df34a573f2f9341f33e167f
Dan Zins <danzins15@gmail.com>
2021-03-22 22:37:44 -0500

    Update JavaScript for page reload test

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -3,6 +3,7 @@ const hamRotate = document.querySelector('#ham-rotate');
 const hamDisplay = document.querySelector('#ham-display');
 const arrow = document.querySelector('#arrow');
 const subUnorderedList = document.querySelector('.subunordered--display');
+// const navIndex = document.querySelector('#nav-main__index');
 
 
 // executing ham rotate/display click function and toggle class
@@ -17,7 +18,14 @@ arrow.addEventListener('click', function () {
     arrow.classList.toggle('arrow-rotate');
 });
 
+// Current URL: https://my-website.com/page_a
+const nextURL = 'https://danz98.github.io/Second-Street/mens-products.html';
+const nextTitle = 'Mens Products Page';
+const nextState = { additionalInformation: 'Updated the URL with JS' };
 
+// This will create a new entry in the browser's history, without reloading
+window.history.pushState(nextState, nextTitle, nextURL);
 
-
+// This will replace the current entry in the browser's history, without reloading
+window.history.replaceState(nextState, nextTitle, nextURL)
 
