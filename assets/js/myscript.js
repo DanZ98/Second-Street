a293e04c2030efc92828d805048ff107619c0c0b
Dan Zins <danzins15@gmail.com>
2021-03-22 22:39:53 -0500

    Updated html and JavaScript file

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -18,14 +18,6 @@ arrow.addEventListener('click', function () {
     arrow.classList.toggle('arrow-rotate');
 });
 
-// Current URL: https://my-website.com/page_a
-const nextURL = 'https://danz98.github.io/Second-Street/mens-products.html';
-const nextTitle = 'Mens Products Page';
-const nextState = { additionalInformation: 'Updated the URL with JS' };
 
-// This will create a new entry in the browser's history, without reloading
-window.history.pushState(nextState, nextTitle, nextURL);
 
-// This will replace the current entry in the browser's history, without reloading
-window.history.replaceState(nextState, nextTitle, nextURL)
 
